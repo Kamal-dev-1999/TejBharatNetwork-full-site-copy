c81c9ece688c6807b9e6ca6ce565aa9f1ea99ab5
kamal-dev-1999 <kamaltripathi1431@gmail.com>
2025-07-07 10:33:41 +0530

    Fix SPA routing with multiple configurations

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,11 +5,14 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
@@ -17,5 +20,9 @@ export default defineConfig({
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+  },
+  preview: {
+    port: 4028,
+    host: true
   }
 });
